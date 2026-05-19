722c1e96611132a361e6b2dee4c48217e710bfba
kovimarci <72648965+kovimarci@users.noreply.github.com>
2026-05-19 13:34:37 +0200

    Change

--- Program.cs
+++ Program.cs
@@ -3,18 +3,43 @@
     public class Program
     {
         static ServerConnection server;
+        static List<Fish> list = [];
         static async Task Main()
         {
             server = new ServerConnection("http://127.1.1.1:3000");
+            bool run = true;
+
+            list = (await server.Get());
+
+            do
+            {
+                Console.Clear();
+
+                Console.ForegroundColor = ConsoleColor.DarkCyan; Console.WriteLine("\n\n\tAll fishes: "); Console.ForegroundColor = ConsoleColor.Gray; list.ForEach(x => Console.WriteLine(x));
+
+                // 1.
+                Console.ForegroundColor = ConsoleColor.DarkCyan; Console.Write("\n\tNumber of fishes: "); Console.ForegroundColor = ConsoleColor.Gray; Console.WriteLine(list.Count());
+                // 3.
+                Console.ForegroundColor = ConsoleColor.DarkCyan; Console.Write("\n\tSum of the fishes' weights: "); Console.ForegroundColor = ConsoleColor.Gray; Console.WriteLine(Math.Round(list.Select(x => x.weight).Sum(), 6) + " kg");
 
 
-            Console.ForegroundColor = ConsoleColor.DarkCyan; Console.WriteLine("\n\n\tAll fishes: "); Console.ForegroundColor = ConsoleColor.Gray; (await server.Get()).ForEach(x => Console.WriteLine(x));
+                Console.ForegroundColor = ConsoleColor.Yellow; Console.WriteLine("\n\t(R) Refresh");
+                Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("\t(Esc) Exit"); Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write("\n\t> ");
 
-            Console.ForegroundColor = ConsoleColor.DarkCyan; Console.Write("\n\tNumber of fishes: "); Console.ForegroundColor = ConsoleColor.Gray; Console.WriteLine((await server.Get()).Count());
-            Console.ForegroundColor = ConsoleColor.DarkCyan; Console.Write("\n\tSum of the fishes' weights: "); Console.ForegroundColor = ConsoleColor.Gray; Console.WriteLine(Math.Round((await server.Get()).Select(x => x.weight).Sum(), 6) + " kg");
-
-
-            Console.ReadKey(false);
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.R:
+                        list = (await server.Get());
+                        break;
+                    case ConsoleKey.Escape:
+                        run = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            while (run);
         }
     }
 }
